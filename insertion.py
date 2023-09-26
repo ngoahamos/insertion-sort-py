3f8daf3742863434271834a95b525ab38c7c0f49
Amos Ngoah <ngoahamos@gmail.com>
2023-09-26 15:44:36 +0800

    code commented

--- insertion.py
+++ insertion.py
@@ -1,19 +1,24 @@
-
+# insertion sort function 
 def insertionSort(arr):
+	#run through the list
 	for i in range(1, len(arr)):
-
+		# key pointer
 		key = arr[i]
 		j = i-1
 		while j >= 0 and key < arr[j] :
+				# perform swap
 				arr[j + 1] = arr[j]
 				j -= 1
+		# update key
 		arr[j + 1] = key
+		# print pass
 		print("Pass " + str(i))
+		# print input data at specific pass 
 		print(arr)
-
-
-
+# get input from user
 data = input("Enter numbers separated by commas: ");
+
+
 try:
     arr = list(map(lambda a: int(a) , data.split(',')))
     print("input data")
